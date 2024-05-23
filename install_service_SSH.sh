675101645b43c04bea6993406e8f623a6b181131
Orlando Mota Pires <orlandomota2002@gmail.com>
2024-05-22 18:41:05 -0700

    Update install_service_SSH.sh

--- install_service_SSH.sh
+++ install_service_SSH.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# Check the status of the SSH service
-sudo systemctl status ssh
+
+# Check if SSH is already installed, if not, install it
+if ! dpkg -l | grep -q openssh-server; then
+    sudo apt update
+    sudo apt install -y openssh-server
+else
+    echo "SSH is already installed."
+fi
 
 # Start the SSH service
 sudo systemctl start ssh
 
 # Enable the SSH service to start on boot
 sudo systemctl enable ssh
+
+# Check the status of the SSH service
+sudo systemctl status ssh
